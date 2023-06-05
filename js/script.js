7a8f2c7366acdbad004c09a31a5e577fdae91c27
martinsir <double_pun@hotmail.com>
2023-06-05 20:39:58 +0200

    ingen menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
-/* navigation hamburgermenu hide and show */
 
-      var navLinks = document.getElementById("navLinks");
-
-      function showMenu() {
-        navLinks.style.right = "0";
-      }
-      function hideMenu() {
-        navLinks.style.right = "-200px";
-      }
   
