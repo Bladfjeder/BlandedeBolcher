4c8e12e9b422d7993781f11e87a2aa3e680f92ce
Bladfjeder <msmmd21@gmail.com>
2023-06-06 18:19:18 +0200

    Opdatering af samtlige sider

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // JavaScript skrevet fra bunden af gruppen "blandede Bolcher", bestående af Ella,  Ahmed, Bastian, Martin & Maiken. 
 
-function hideMe(){
-    
-    
 
+// Knap kode er venligst lånt af Maiken Ø. Sørensen's eksamensprojekt på første semester.
 
+// TIL TOPPEN KNAP 
+//først henter vi knappen fra html ved at kalde på dens ID
+let topknap = document.getElementById("topKnap");
+
+//Så fortæller vi computeren at når man scroller skal den aktivere funktionen
+window.onscroll = function() {scrollfunktion()};
+
+//Her skriver vi en regel om hvad vores scroll funktion skal gøre
+function scrollfunktion() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+            { topknap.style.display = "block"; }
+    else
+            { topknap.style.display = "none"; }
+}
+
+//Her skriver vi en regel om hvor knappen tager dig hen når du trykker
+function tilTopFunktion() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
 }
